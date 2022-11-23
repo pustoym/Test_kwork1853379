7d42d0afe61daffedb5b3095d59d39429f3c6a44
Matvey P <pustoy.dev@gmail.com>
2022-11-23 09:52:43 +0300

    🐣 Init

---
+++ source/vendor.js
@@ -0,0 +1,8 @@
+// Swiper 7.4.1
+// import './vendor/swiper';
+// import './vendor/focus-visible-polyfill';
+import './vendor/poper.js';
+import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
